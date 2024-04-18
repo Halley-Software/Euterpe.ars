da61a89bbb814614fcc30762c8217e47f7abfc85
Raxabi <92801687+Raxabi@users.noreply.github.com>
2024-04-18 09:47:43 +0200

    [MOVE] moved all svgs and icons to 'svgs' directory

---
+++ client/src/components/svgs/react/PreviousButton.tsx
@@ -0,0 +1,33 @@
+import "../styles/PlayerControls.css"
+
+function PreviousButton() {
+  return (
+    <svg
+      id="back"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path
+        className="svg-control-hover-color"
+        d="M19.496 4.136l-12 7a1 1 0 0 0 0 1.728l12 7a1 1 0 0 0 1.504 -.864v-14a1 1 0 0 0 -1.504 -.864z"
+        strokeWidth="0"
+        fill="#9ba9be"
+      />
+      <path
+        className="svg-control-hover-color"
+        d="M4 4a1 1 0 0 1 .993 .883l.007 .117v14a1 1 0 0 1 -1.993 .117l-.007 -.117v-14a1 1 0 0 1 1 -1z"
+        strokeWidth="0"
+        fill="#9ba9be"
+      />
+    </svg>
+  )
+}
+
+export default PreviousButton
