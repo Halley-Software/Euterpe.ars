f667dff6ef96a1a18b34e9b215b26cb0b5f496b7
Raxabi <92801687+Raxabi@users.noreply.github.com>
2024-04-18 09:50:11 +0200

    [ADD] save a new song behaviour implemented

--- client/src/global/CONSTANTS.ts
+++ client/src/global/CONSTANTS.ts
@@ -7,11 +7,15 @@
 export const VOLUME_MAX_DIVIDER = 300
 
 export const ADDR =
+    // "192.168.1.57"
     "192.168.1.139"
     // "192.168.5.103"
 
 export const MUSIC_NOT_FOUND_MESSAGES: string[] = [
     "We have not found songs to be played yet! So we are singing them ;)",
-    "Maybe u will need to sing yout own songs",
+    "Maybe u will need to sing your own songs",
     "No music to be played. Stage your own musical group and start play music"
 ]
+
+// Regular expression form https://github.com/Raxabi/Downlotube-HTTP/blob/main/client/src/components/UrlForm.tsx#L15C92-L15C92
+export const VALID_URL = new RegExp(/^(https:\/\/(www\.)?)?youtube\.com\/watch\?v=.|youtu\.be\/./)
