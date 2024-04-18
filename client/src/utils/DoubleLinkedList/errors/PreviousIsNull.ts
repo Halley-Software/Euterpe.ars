e4578dc1b1ed59918f5394a39247017b039cf89c
Raxabi <92801687+Raxabi@users.noreply.github.com>
2024-04-18 10:04:53 +0200

    [CHANGE] replaced or (||) operator to nullish (??) operator

--- client/src/utils/DoubleLinkedList/errors/PreviousIsNull.ts
+++ client/src/utils/DoubleLinkedList/errors/PreviousIsNull.ts
@@ -1,5 +1,5 @@
 export class PreviousIsNullError extends Error {
   public constructor(message: string, cause?: string) {
-    super(message, { cause: cause || "List is empty" })
+    super(message, { cause: cause ?? "List is empty" })
   }
 }
